f93de6ea3412883b796128c5374dcb5185051228
Naina Subherwal <nainasubherwal@gmail.com>
2025-07-24 09:37:02 -0700

    Create root view

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -8,16 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        
+        NavigationStack {
+            
+            VStack {
+                
+                Text("This is the root view 🌳")
+                    .font(.largeTitle)
+                
+            }//vstack
+            
+        }//navigationstack
+        
+    }//body
+    
+}//struct
+
 
 #Preview {
     ContentView()
